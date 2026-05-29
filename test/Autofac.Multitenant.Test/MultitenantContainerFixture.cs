f0034c26f5a688d41cd0164bb7fb1109664d8bb7
Travis Illig <tillig@paraesthesia.com>
2026-05-29 10:19:22 -0700

    Fix analyzer issues.

--- test/Autofac.Multitenant.Test/MultitenantContainerFixture.cs
+++ test/Autofac.Multitenant.Test/MultitenantContainerFixture.cs
@@ -1168,7 +1168,7 @@ public class MultitenantContainerFixture
         {
             TenantId = "tenant1",
         };
-        var mtc = new MultitenantContainer(strategy, new ContainerBuilder().Build());
+        await using var mtc = new MultitenantContainer(strategy, new ContainerBuilder().Build());
         mtc.ConfigureTenant("tenant1", b => b.RegisterType<StubDependency1Impl1>().As<IStubDependency1>());
 
         var exception = await Record.ExceptionAsync(async () => await mtc.DisposeAsync());
